474d3b6400ff44ba13fb80388e09eaec55a25dcd
Luiz Henrique Lobo <luizhenriquewolf@gmail.com>
2023-06-13 22:31:50 -0300

    Atualizando forma de mostrar dados quando pequisa por disciplina específica

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,14 @@
+export const minNumberOfSchedules = 6;
+
 export function getSchedule(data, disciplineId) {
   delete data.bloco;
   const dataArray = Object.values(data);
-  const schedule = [];
+  const schedules = [];
   for (const room of dataArray) {
     for (const [_, classItem] of room.classes.entries()) {
       for (let i = 0; i < classItem.length; i++) {
         if (classItem[i].includes(disciplineId)) {
-          schedule.push({
+          schedules.push({
             discipline: classItem[i],
             dayOfWeek: getDayOfWeek(i),
             time: classItem[0],
@@ -17,7 +19,46 @@ export function getSchedule(data, disciplineId) {
       }
     }
   }
-  return schedule;
+  if (schedules.length <= minNumberOfSchedules) {
+    return customizeSchedules(schedules);
+  } else {
+    return schedules;
+  }
+}
+
+function customizeSchedules(schedules) {
+  const customizeSchedules = [];
+  const tempData = {};
+  for (const data of schedules) {
+    const key = data.dayOfWeek;
+    if (tempData[key]) {
+      tempData[key].time = tempData[key].time.split(' - ')[0] + ' - ' + data.time.split(' - ')[1];
+    } else {
+      tempData[key] = { ...data };
+    }
+  }
+  for (const key in tempData) {
+    customizeSchedules.push(tempData[key]);
+  }
+  return sortSchedulesByDayOfWeek(customizeSchedules);
+}
+
+function sortSchedulesByDayOfWeek(simplifiedSchedules) {
+  simplifiedSchedules.sort((a, b) => {
+    const daysOfWeek = [
+      'Domingo',
+      'Segunda-feira',
+      'Terça-feira',
+      'Quarta-feira',
+      'Quinta-feira',
+      'Sexta-feira',
+      'Sábado',
+    ];
+    const dayA = daysOfWeek.indexOf(a.dayOfWeek);
+    const dayB = daysOfWeek.indexOf(b.dayOfWeek);
+    return dayA - dayB;
+  });
+  return simplifiedSchedules;
 }
 
 function getDayOfWeek(index) {
