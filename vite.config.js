52f6c8ab54a4c7ef3e3aeb995d24811f71e20f2f
Luiz Henrique Lobo <luizhenriquewolf@gmail.com>
2024-03-15 17:31:32 -0300

    Adicionando possibilidade de baixar aplicação como PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Find Your Class',
+        short_name: 'FYC',
+        description: 'Um simples app para consultar os horários e locais das suas aulas.',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'apple touch icon',
+          },
+          {
+            src: '/maskable_icon.png',
+            sizes: '225x225',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#5a54f9',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+      },
+    }),
+  ],
 });
